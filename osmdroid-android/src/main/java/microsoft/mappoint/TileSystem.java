f96e89dcfbc2bd12ecf8eae895122245096d9774
Hyuntae Kim <>
2019-09-19 13:27:00 +0200

    commit for test

--- osmdroid-android/src/main/java/microsoft/mappoint/TileSystem.java
+++ osmdroid-android/src/main/java/microsoft/mappoint/TileSystem.java
@@ -18,6 +18,7 @@ import android.graphics.Point;
  */
 public final class TileSystem {
 
+	private int test2 = 0;
 	protected static int mTileSize = 256;
 	private static final double EarthRadius = 6378137;
 	private static final double MinLatitude = -85.05112878;
